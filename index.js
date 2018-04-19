a013a34db98c7663ad5af15a0b804dc05b266443
churei <churei@arssystem.co.jp>
2018-04-19 05:16:33 +0000

    全ての投稿内容の表示

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const http = require('http');
 const router = require('./lib/router');
 
 const server = http.createServer((req, res) => {
-  router.route(req, res);
+    router.route(req, res);
 }).on('error', (e) => {
-  console.error('Server Error', e);
+    console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('Client Error', e);
+    console.error('Client Error', e);
 });
 
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+    console.info('Listening on ' + port);
 });
