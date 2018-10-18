cc0ed9a2c1439dc823d0a4940a5361d5527cf1a1
EC2 Default User <ec2-user@ip-172-31-47-197.ap-northeast-1.compute.internal>
2018-10-18 12:15:52 +0000

    投稿昨日

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
+//httpをrequireする
 const http = require('http');
+//モジュールを作る
 const router = require('./lib/router');
+//serverモジュールを作る
 
 const server = http.createServer((req, res) => {
+//リクエストが来た時にrouteを呼び出す
+	console.info('call route');
   router.route(req, res);
+
 }).on('error', (e) => {
+//errorイベントを受信したらコンソールに出す
   console.error('Server Error', e);
 }).on('clientError', (e) => {
   console.error('Client Error', e);
