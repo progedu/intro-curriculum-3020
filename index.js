b7ad03129654eb569ad4c2a7cf06703f0189da76
RyuPC <vbkit@outlook.com>
2020-04-01 13:46:03 +0900

    投稿内容を保存する機能の実装

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   console.error('Client Error', e);
 });
 
-const port = 8000;
+const port = 8002;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
