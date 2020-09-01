128f2fae915103ac0657afd26fd2035a70540c8c
mumu\chacha <mutsuki.cocoa@gmail.com>
2020-09-02 01:16:15 +0900

    全投稿内容を保存する機能の実装

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 const http = require('http');
-const router = require('./lib/router');
+const router = require('./lib/router');//作業ディレクトリを基準として相対パスを指定。モジュールを require で読み込む際には、.js は省略可
 
 const server = http.createServer((req, res) => {
-  router.route(req, res);
+  router.route(req, res);//route という関数で必要なリクエストの振り分け処理を行ってくれる
 }).on('error', (e) => {
   console.error('Server Error', e);
 }).on('clientError', (e) => {
@@ -12,5 +12,5 @@ const server = http.createServer((req, res) => {
 
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info(`ポート ${port} 番でサーバーを起動しました`);
 });
